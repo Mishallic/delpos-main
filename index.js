c11277abda3dac1d02806d68cb9f4a5fda7276dd
Mishallic <mish.al.shomary@gmail.com>
2019-05-23 13:06:15 +0300

    'pls'

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ if(process.env.NODE_ENV === 'production'){
         res.sendFile(path.join(__dirname+'/delpos-client/build/index.html'));
       });
 }
-
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"))
+})
 app.get('/api/mail', (req, res) => {
     res.json(req.body)
 })
