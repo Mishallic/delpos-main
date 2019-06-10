b2dedae0d57dcbab8bda6c12cc4d768f8692c173
Mishallic <mish.al.shomary@gmail.com>
2019-06-10 16:28:29 +0300

    'fixed-client-node'

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ if(process.env.NODE_ENV === 'production'){
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname,'client','build','index.html'));
         console.log('indexing');
-        
       });
 }
 
