8a834b7956ec39c19f63420f86ff902663e0228e
Mishallic <mish.al.shomary@gmail.com>
2019-06-10 17:31:13 +0300

    'server-fix'

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static('server/client/build'))
 
 if(process.env.NODE_ENV === 'production'){
     app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'/client','build','index.html'))
         console.log('indexing');
       });
 }
