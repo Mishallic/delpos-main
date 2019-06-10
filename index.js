bfcada4933a349334d979afa935d47209a360939
Mishallic <mish.al.shomary@gmail.com>
2019-06-10 17:47:27 +0300

    'final'

--- index.js
+++ index.js
@@ -11,14 +11,18 @@ const port = process.env.PORT || 4444;
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
-app.use(express.static('client/build'))
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname,'client','build','index.html'))
+        res.sendfile(path.join(__dirname = 'client/build/index.html'))
       });
 }
 
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname+'/client/public/index.html'))
+})
 
 app.get('/api/mail', (req, res) => {
     res.json(req.body)
