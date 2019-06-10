53a6d1082144466cb028b265d87d1ce9ef5a6caf
Mishallic <mish.al.shomary@gmail.com>
2019-06-10 17:35:33 +0300

    'dirname'

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static('/client/build'))
 
 if(process.env.NODE_ENV === 'production'){
     app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname,'/client','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'./client','build','index.html'))
       });
 }
 
