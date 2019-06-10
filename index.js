37136f90ac7f1f301a4c5a1f627e87898cdf0a5a
Mishallic <mish.al.shomary@gmail.com>
2019-06-10 16:52:02 +0300

    'fixed-server'

--- index.js
+++ index.js
@@ -12,15 +12,9 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, 'client','build','static')));
+app.use(express.static('client/build'))
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, 'client/build/static')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname,'client','build','index.html'));
-        console.log('indexing');
-      });
-}
+
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'client','build', "index.html"))
@@ -44,7 +38,7 @@ app.post('/api/mail', (req,res) => {
 })
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'client','build','index.html'));
+    res.sendFile('client/build/index.html')
   });
 app.listen( port, () => {
     console.log('live on port 4444')
