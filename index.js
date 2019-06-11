3048467821567eb609a0cf2b727c37239c168c1e
Mishallic <mish.al.shomary@gmail.com>
2019-06-11 09:06:24 +0300

    'plss'

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ if(process.env.NODE_ENV === 'production'){
         res.sendFile(path.join(__dirname,'client', 'build', 'index.html'))
       });
 }
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname,'client', 'build', 'index.html'))
+  });
 
 app.get('/api/mail', (req, res) => {
     res.json(req.body)
