49a00c0d6b67fdb3922307876c4441f61673d5bb
Mishallic <mish.al.shomary@gmail.com>
2019-06-11 11:03:08 +0300

    'good'

--- index.js
+++ index.js
@@ -13,15 +13,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 app.use(express.static(path.join(__dirname, './client/build')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname ,'./client', 'build','index.html'))
-  })
+
 
   if (process.env.NODE_ENV === "production") {
-    app.use('/', express.static("client/build"));
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use('/', express.static("/client/build"));
+    app.use(express.static(path.join(__dirname, '/client/build')));
     app.get('/*', function (req, res) {
-        res.sendFile(path.join(__dirname,'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname,'/client', 'build', 'index.html'));
     });
   }
 
