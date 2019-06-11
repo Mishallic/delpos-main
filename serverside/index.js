77e47172d8eb1fa3a789fabd8a2468f870f11846
Mishallic <mish.al.shomary@gmail.com>
2019-06-11 08:46:20 +0300

    'api'

--- serverside/index.js
+++ serverside/index.js
@@ -16,12 +16,12 @@ app.use(express.static(path.join(__dirname, 'client/build')))
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('/*', (req, res) => {
-        res.sendfile(path.join(__dirname = 'client/build/index.html'))
+        res.sendfile(path.join(__dirname ,'client','build','index.html'))
       });
 }
 
-app.get('*', (req,res)=>{
-    res.sendFile(path.join(__dirname+'/client/public/index.html'))
+app.get('/*', (req,res)=>{
+    res.sendFile(path.join(__dirname+'client/public/index.html'))
 })
 
 app.get('/api/mail', (req, res) => {
