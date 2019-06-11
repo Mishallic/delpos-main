63e60a50fb5458606ab8a44174f93b4ff9f7e131
Mishallic <mish.al.shomary@gmail.com>
2019-06-11 10:12:59 +0300

    'awesome'

--- index.js
+++ index.js
@@ -5,23 +5,24 @@ const cors = require('cors')
 const path = require('path');
 const app = express();
 
+
 SGmail.setApiKey('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')
 const port = process.env.PORT || 4444;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname , 'build','index.html'))
+    res.sendFile(path.join(__dirname ,'./clent', 'build','index.html'))
   })
 
   if (process.env.NODE_ENV === "production") {
-    app.use('/', express.static("client/build"));
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use('/', express.static("./client/build"));
+    app.use(express.static(path.join(__dirname, './client/build')));
     app.get('/*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        res.sendFile(path.join(__dirname,'./client', 'build', 'index.html'));
     });
   }
 
