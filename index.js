0819116979f35ed4efb6b0e373e3db4504f76546
Mishallic <mish.al.shomary@gmail.com>
2019-06-11 09:33:12 +0300

    'fixed-route'

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'client/build')))
 
-
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+    res.sendFile(path.join(__dirname + 'build','index.html'))
   })
 
 app.get('/api/mail', (req, res) => {
