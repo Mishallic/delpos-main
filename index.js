58d46257be951bfa75773a75c9eda2253915ec2f
Mishallic <mish.al.shomary@gmail.com>
2019-06-11 09:02:08 +0300

    'pls'

--- index.js
+++ index.js
@@ -14,9 +14,8 @@ app.use(cors())
 app.use(express.static(path.join(__dirname, 'client/build')))
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', (req, res) => {
-        res.sendfile(path.join(__dirname +'/client/build/index.html'))
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname,'client', 'build', 'index.html'))
       });
 }
 
